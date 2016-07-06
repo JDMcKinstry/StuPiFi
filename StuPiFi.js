59789888060e11f3ccfe70609a688481cd5a1234
JD McKinstry <jdmckinstry@gmail.com>
2016-07-06 15:40:05 -0400

    Add files via upload

--- StuPiFi.js
+++ StuPiFi.js
@@ -92,12 +92,12 @@
 			'a': function() { return this.getHours() >= 12 ? "pm" : "am"; },
 			'A': function() { return this.getHours() >= 12 ? "PM" : "AM"; },
 			'B': function() { return "@"+("00"+Math.floor((((this.getHours()+1)%24*60+this.getMinutes())*60+this.getSeconds()+(this.getMilliseconds()*0.001))/86.4)).slice(-3); },
-			'g': function() { var a = this.getHours(); return a <= 12 ? a : a - 12; },
-			'G': function() { return this.getHours(); },
-			'h': function() { var a = this.getHours(); a = a <= 12 ? a : a - 12; return a > 9 ? a : '0' + a; },
-			'H': function() { var a = this.getHours(); return a > 9 ? a : '0' + a; },
-			'i': function() { var a = this.getMinutes(); return a > 9 ? a : '0' + a; },
-			's': function() { var a = this.getSeconds(); return a > 9 ? a : '0' + a; },
+			'g': function() { var a = this.getHours(); return a <= 12 ? a : a - 12; },	//	12-hour format of an hour without leading zeros
+			'G': function() { return this.getHours(); },	//	24-hour format of an hour without leading zeros
+			'h': function() { var a = this.getHours(); a = a <= 12 ? a : a - 12; return a > 9 ? a : '0' + a; },	//		12-hour format of an hour with leading zeros
+			'H': function() { var a = this.getHours(); return a > 9 ? a : '0' + a; },	//		24-hour format of an hour with leading zeros
+			'i': function() { var a = this.getMinutes(); return a > 9 ? a : '0' + a; },	//	Minutes with leading zeros
+			's': function() { var a = this.getSeconds(); return a > 9 ? a : '0' + a; },	//	Seconds, with leading zeros
 			'u': function() { return this.getMilliseconds(); },	//	this is NOT microseconds ... it's JS :P,
 			/*	TIMEZONE	*/
 			'e': function() { var a = this.toString().match(/ ([A-Z]{3,4})([-|+]?\d{4})/); return a.length > 1 ? a[1] : ''; },
@@ -114,33 +114,124 @@
 			/*	FULL DATE/TIME	*/
 			'c': function() { return new Date(this).addHours(-(this.getTimezoneOffset() / 60)).toISOString(); },
 			'r': function() { return new Date(this).addHours(-(this.getTimezoneOffset() / 60)).toISOString(); },
-			'U': function() { return this.getTime() / 1000; }
+			'U': function() { return this.getTime() / 1000 | 0; }
+		},
+		compound = {
+			'commonLogFormat': 'd/M/Y:G:i:s',
+			'exif': 'Y:m:d G:i:s',
+			'isoYearWeek': 'Y\\WW',
+			'isoYearWeek2': 'Y-\\WW',
+			'isoYearWeekDay': 'Y\\WWj',
+			'isoYearWeekDay2': 'Y-\\WW-j',
+			'mySQL': 'Y-m-d h:i:s',
+			'postgreSQL': 'Y.z',
+			'postgreSQL2': 'Yz',
+			'soap': 'Y-m-d\\TH:i:s.u',
+			'soap2': 'Y-m-d\\TH:i:s.uP',
+			'unixTimestamp': '@U',
+			'xmlrpc': 'Ymd\\TG:i:s',
+			'xmlrpcCompact': 'Ymd\\tGis',
+			'wddx': 'Y-n-j\\TG:i:s'
+		},
+		constants = {
+			'AMERICAN': 'F j, Y',
+			'AMERICANSHORT': 'm/d/Y',
+			'AMERICANSHORTWTIME': 'm/d/Y h:i:sA',
+			'ATOM': 'Y-m-d\TH:i:sP',
+			'COOKIE': 'l, d-M-Y H:i:s T',
+			'EUROPEAN': 'j F Y',
+			'EUROPEANSHORT': 'd.m.Y',
+			'EUROPEANSHORTWTIME': 'd.m.Y H:i:s',
+			'ISO8601': 'Y-m-d\TH:i:sO',
+			'LEGAL': 'j F Y',
+			'RFC822': 'D, d M y H:i:s O',
+			'RFC850': 'l, d-M-y H:i:s T',
+			'RFC1036': 'D, d M y H:i:s O',
+			'RFC1123': 'D, d M Y H:i:s O',
+			'RFC2822': 'D, d M Y H:i:s O',
+			'RFC3339': 'Y-m-d\TH:i:sP',
+			'RSS': 'D, d M Y H:i:s O',
+			'W3C': 'Y-m-d\TH:i:sP'
 		}
 	
+	//	utc param currently does nothing except bool false to return list of constants as pure strings (no format)
 	function dateFormat(str, utc) {	//	string, boolean TODO: use utc bool to determine in converter methods if to use UTC version
 		if (str) {
-			var ret = str.split('');
-			for (var x in ret) {
-				if (ret[x] && /[a-z]/i.test(ret[x])) {
-					var rx = new RegExp(ret[x], 'g');
-					ret[x] = formats[ret[x]] ? formats[ret[x]].apply(this) : ret[x];
-				}
+			if (str == 'compound') {
+				if (utc === false) return this.format.compound;
+				var r = {};
+				for (var x in Date.prototype.format.compound) r[x] = this.format(Date.prototype.format.compound[x]);
+				return r;
 			}
-			return ret.join('');
+			else if (Date.prototype.format.compound[str]) return this.format(Date.prototype.format.compound[str]);
+			
+			if (str == 'constants') {
+				if (utc === false) return this.format.constants;
+				var r = {};
+				for (var x in Date.prototype.format.constants) r[x] = this.format(Date.prototype.format.constants[x]);
+				return r;
+			}
+			else if (Date.prototype.format.constants[str]) return this.format(Date.prototype.format.constants[str]);
+			
+			var ret = str.split(''), lc = '';
+			for (var x in ret) {
+				var c = ret[x];
+				if ((c && /[a-z]/i.test(c)) && !(/\\/.test(lc + c))) {
+					var rx = new RegExp(c, 'g');
+					ret[x] = formats[c] ? formats[c].apply(this) : c;
+				}
+				lc = ret[x];
+			}
+			return ret.join('').replace(/\\/, '');
 		}
 		return str;
 	}
-	dateFormat.test = function() {
+	
+	function dateFormatTest() {
 		var a = 'd D j l N S w z W F m M n t L o Y y a A B g G h H i s u e I O P T Z c r U'.split(' '),
-			d = new Date();
+			d = new Date(), r = {};
 		for (var x in a) {
 			var b = a[x], f = d.format(b);
-			console.log("["+b+"]:\t%c\""+f+"\"", "color:red;");
+			if (console && console.log) console.log("["+b+"]:\t%c\""+f+"\"", "color:red;");
+			r[x] = f;
 		}
+		return r;
 	}
 	
-	Object['defineProperty'] && !Date.prototype.hasOwnProperty('format')
-			? Object.defineProperty(Date.prototype, 'format', { value: dateFormat }) : Date.prototype['format'] = dateFormat;
+	function dateFormatCompoundTest() {
+		var a = Date.prototype.format.compound, d = new Date(), r = {};
+		for (var x in a) {
+			var b = a[x];
+			if (typeof b == 'string') {
+				var f = d.format(b);
+				if (console && console.log) console.log("["+x+"] '"+b+"':\t%c\""+f+"\"", "color:red;");
+				r[x] = f;
+			}
+		}
+		return r;
+	}
+	
+	function dateFormatConstantsTest() {
+		var a = Date.prototype.format.constants, d = new Date(), r = {};
+		for (var x in a) {
+			var b = a[x];
+			if (typeof b == 'string') {
+				var f = d.format(b);
+				if (console && console.log) console.log("["+x+"] '"+b+"':\t%c\""+f+"\"", "color:red;");
+				r[x] = f;
+			}
+		}
+		return r;
+	}
+	
+	Object['defineProperty'] ? Object.defineProperty(compound, 'test', { value: dateFormatCompoundTest }) : compound['test'] = dateFormatCompoundTest;
+	Object['defineProperty'] ? Object.defineProperty(constants, 'test', { value: dateFormatConstantsTest }) : constants['test'] = dateFormatConstantsTest;
+	
+	Object['defineProperty'] ? Object.defineProperty(dateFormat, 'test', { value: dateFormatTest }) : dateFormat['test'] = dateFormatTest;
+	Object['defineProperty'] ? Object.defineProperty(dateFormat, 'compound', { value: compound }) : dateFormat['compound'] = compound;
+	Object['defineProperty'] ? Object.defineProperty(dateFormat, 'constants', { value: constants }) : dateFormat['constants'] = constants;
+	
+	Object['defineProperty'] && !Date.prototype.hasOwnProperty('format') ? Object.defineProperty(Date.prototype, 'format', { value: dateFormat }) : Date.prototype['format'] = dateFormat;
 })();
 
 /**	Element[extensions]()
